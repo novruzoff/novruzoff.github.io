51323204fd0b8504ac67fe10b81f67d3e3f8e903
Novruzoff <95917545+Novruzoff@users.noreply.github.com>
2024-05-24 02:12:06 +0400

    0.3.14

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-// src/App.js
 import React, { Suspense, lazy } from 'react';
 import './App.css';
 import Header from './components/Header';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const Home = lazy(() => import('./components/Home'));
 const About = lazy(() => import('./components/About'));
