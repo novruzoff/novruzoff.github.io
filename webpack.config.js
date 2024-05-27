2e31add8035c4d4280ea612345158be03b75a0fc
Novruzoff <95917545+Novruzoff@users.noreply.github.com>
2024-05-27 23:43:44 +0400

    0.6.13

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (env, argv) => {
   return {
     entry: './src/index.js',
     output: {
-      path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname, 'build'),
       filename: isProduction ? '[name].[contenthash].js' : '[name].bundle.js',
       publicPath: '/',
     },
