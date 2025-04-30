256c9ca55390adb1014f50c6e8ebc7b8b8d78f66
novruzoff <murad.novruzov1899@gmail.com>
2025-04-30 03:00:11 -0400

    seo: wrap the app in react-helmet-async

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { HelmetProvider } from 'react-helmet-async';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>,
+  document.getElementById('root')
+);
